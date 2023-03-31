e3438c2b987612136e77839922ed2519bacd01a3
taejun <joon5006@naver.com>
2023-03-31 20:11:20 +0900

    fix: ormconfig.ts cil 부분 삭제

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,23 +1,18 @@
-import { ConnectionOptions } from "typeorm";
+import { ConnectionOptions } from 'typeorm';
 
 export const ormconfig: ConnectionOptions = {
-  type: "mysql",
+  type: 'mysql',
   host: process.env.HOST,
   port: Number(process.env.MYSQL_PORT),
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   synchronize: true,
-  logging: ["warn", "error"],
-  charset: "utf8mb4_unicode_ci",
-  entities: [__dirname + "/entities/*.*"],
-  migrations: [__dirname + "/migrations/*.*"],
-  subscribers: [__dirname + "/subscribers/*.*"],
-  cli: {
-    entitiesDir: __dirname + "/entities",
-    migrationsDir: "/src/migrations",
-    subscribersDir: __dirname + "/subscribers",
-  },
+  logging: ['warn', 'error'],
+  charset: 'utf8mb4_unicode_ci',
+  entities: [__dirname + '/entities/*.*'],
+  migrations: [__dirname + '/migrations/*.*'],
+  subscribers: [__dirname + '/subscribers/*.*'],
 };
 
 export default ormconfig;
