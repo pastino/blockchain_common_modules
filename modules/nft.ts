a95a809e85de0e94f89194f5ff006cded5c3023a
taejun <joon5006@naver.com>
2023-06-11 17:55:12 +0900

    fix: trait type 없는 경우 continue

--- modules/nft.ts
+++ modules/nft.ts
@@ -98,6 +98,7 @@ export class NFT {
       queryRunner.manager.getRepository(TraitTypeContract);
 
     for (const attributeData of attributesData) {
+      if (!attributeData.trait_type) continue;
       // TraitType이 이미 생성되었는지 확인 후, 없으면 생성
       let traitType = await traitTypeRepo.findOne({
         traitType: attributeData.trait_type,
