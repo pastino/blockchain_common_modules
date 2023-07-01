06b2d5c3a4c8c6873a39f84045642de34f9678fe
taejun <joon5006@naver.com>
2023-07-01 15:53:16 +0900

    fix: DecodedLog 테이블 수정 - example 추가

--- entities/DecodedLog.ts
+++ entities/DecodedLog.ts
@@ -8,12 +8,22 @@ import {
   JoinColumn,
   ManyToOne,
   Index,
+  AfterLoad,
 } from "typeorm";
 import { Action } from "../modules/decodeLog";
 import { Log } from "./Log";
 import { Transaction } from "./Transaction";
 import { Contract } from "./Contract";
 import { NFT } from "./NFT";
+import * as dotenv from "dotenv";
+import { decodedLogExample } from "../entityExamples";
+
+dotenv.config({ path: __dirname + "/../../../.env.dev" });
+const isNestJs = process.env.APP_TYPE === "nestjs";
+
+const ApiProperty = isNestJs
+  ? require("@nestjs/swagger").ApiProperty
+  : () => {};
 
 @Entity({ name: "decodedLog" })
 @Index("idx_decodedlog_eventTime", ["eventTime"])
@@ -112,4 +122,251 @@ export class DecodedLog {
   createdAt: Date;
   @UpdateDateColumn()
   updatedAt: Date;
+
+  transactionFee: string;
+  @AfterLoad()
+  convertGasPrice() {
+    if (this.gasPrice && this.effectiveGasPrice) {
+      const fee =
+        (parseFloat(this.effectiveGasPrice) * parseFloat(this.gasUsed)) /
+        Math.pow(10, 18);
+      this.transactionFee = fee.toFixed(4);
+    }
+  }
+
+  static example(): DecodedLog {
+    const instance: any = new DecodedLog();
+
+    for (let key in decodedLogExample) {
+      instance[key] = decodedLogExample[key];
+    }
+
+    return instance;
+  }
+}
+
+if (isNestJs) {
+  const {
+    id,
+    log,
+    transaction,
+    contract,
+    nft,
+    action,
+    contractAddress,
+    tokenId,
+    from,
+    to,
+    ethValue,
+    unit,
+    value,
+    platform,
+    quantity,
+    minterAddress,
+    stage,
+    mintCount,
+    timestamp,
+    eventTime,
+    gasUsed,
+    cumulativeGasUsed,
+    effectiveGasPrice,
+    gasPrice,
+    gasLimit,
+    createdAt,
+    updatedAt,
+    transactionFee,
+  } = decodedLogExample;
+
+  const propertyDecorators = [
+    ApiProperty({
+      name: "id",
+      type: Number,
+      example: id,
+      description: "Uniqe ID",
+    }),
+    // ApiProperty({
+    //   name: 'log',
+    //   type: log,
+    //   example: log,
+    //   description: '로그 데이터',
+    // }),
+
+    // ApiProperty({
+    //   name: 'transaction',
+    //   type: transaction,
+    //   example: transaction,
+    //   description: '트랜잭션 데이터',
+    // }),
+
+    // ApiProperty({
+    //   name: 'contract',
+    //   type: contract,
+    //   example: contract,
+    //   description: '컨트랙트 데이터',
+    // }),
+
+    // ApiProperty({
+    //   name: 'nft',
+    //   type: nft,
+    //   example: nft,
+    //   description: 'NFT 데이터',
+    // }),
+
+    ApiProperty({
+      name: "action",
+      type: String,
+      example: action,
+      description: "액션(Sale | Transfer | Mint)",
+    }),
+
+    ApiProperty({
+      name: "contractAddress",
+      type: String,
+      example: contractAddress,
+      description: "컬랙션 주소",
+    }),
+
+    ApiProperty({
+      name: "tokenId",
+      type: String,
+      example: tokenId,
+      description: "토큰 아이디",
+    }),
+
+    ApiProperty({
+      name: "from",
+      type: String,
+      example: from,
+      description: "From 주소",
+    }),
+
+    ApiProperty({
+      name: "to",
+      type: String,
+      example: to,
+      description: "To 주소",
+    }),
+
+    ApiProperty({
+      name: "ethValue",
+      type: String,
+      example: ethValue,
+      description: "가격(ETH)",
+    }),
+    ApiProperty({
+      name: "unit",
+      type: String,
+      example: unit,
+      description: "단위",
+    }),
+    ApiProperty({
+      name: "value",
+      type: String,
+      example: value,
+      description: "가격(ETH)",
+    }),
+
+    ApiProperty({
+      name: "platform",
+      type: String,
+      example: platform,
+      description: "마켓 플래이스",
+    }),
+
+    ApiProperty({
+      name: "quantity",
+      type: Number,
+      example: quantity,
+      description: "수량",
+    }),
+
+    ApiProperty({
+      name: "minterAddress",
+      type: String,
+      example: minterAddress,
+      description: "minterAddress",
+    }),
+
+    ApiProperty({
+      name: "stage",
+      type: String,
+      example: stage,
+      description: "stage",
+    }),
+
+    ApiProperty({
+      name: "mintCount",
+      type: Number,
+      example: mintCount,
+      description: "민팅 갯수",
+    }),
+
+    ApiProperty({
+      name: "timestamp",
+      type: Number,
+      example: timestamp,
+      description: "이벤트 타임스탬프",
+    }),
+
+    ApiProperty({
+      name: "eventTime",
+      type: Date,
+      example: eventTime,
+      description: "이벤트 타임",
+    }),
+
+    ApiProperty({
+      name: "gasUsed",
+      type: String,
+      example: gasUsed,
+      description: "gasUsed",
+    }),
+    ApiProperty({
+      name: "cumulativeGasUsed",
+      type: String,
+      example: cumulativeGasUsed,
+      description: "cumulativeGasUsed",
+    }),
+    ApiProperty({
+      name: "effectiveGasPrice",
+      type: String,
+      example: effectiveGasPrice,
+      description: "effectiveGasPrice",
+    }),
+
+    ApiProperty({
+      name: "gasPrice",
+      type: String,
+      example: gasPrice,
+      description: "gasPrice",
+    }),
+    ApiProperty({
+      name: "transactionFee",
+      type: String,
+      example: transactionFee,
+      description: "transactionFee",
+    }),
+    ApiProperty({
+      name: "gasPrice",
+      type: String,
+      example: gasPrice,
+      description: "gasPrice",
+    }),
+    ApiProperty({
+      name: "createdAt",
+      type: Date,
+      example: createdAt,
+      description: "생성된 시간",
+    }),
+    ApiProperty({
+      name: "updatedAt",
+      type: Date,
+      example: updatedAt,
+      description: "업데이트된 시간",
+    }),
+  ];
+
+  propertyDecorators.forEach((decorator, index) => {
+    decorator(DecodedLog.prototype, index.toString());
+  });
 }
--- migrations/1687660781510-migrations.ts
+++ migrations/1687660781510-migrations.ts
@@ -1,20 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class migrations1687660781510 implements MigrationInterface {
-    name = 'migrations1687660781510'
+export class CreateIndexOnDecodedLogTimestampDESC1622810292473
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE INDEX IDX_DECODEDLOG_TIMESTAMP_DESC ON \`decodedLog\` (\`timestamp\` DESC)`
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `upcomingContract` DROP COLUMN `preSalePrice`");
-        await queryRunner.query("ALTER TABLE `upcomingContract` ADD `preSalePrice` float NULL");
-        await queryRunner.query("ALTER TABLE `upcomingContract` DROP COLUMN `publicSalePrice`");
-        await queryRunner.query("ALTER TABLE `upcomingContract` ADD `publicSalePrice` float NULL");
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `upcomingContract` DROP COLUMN `publicSalePrice`");
-        await queryRunner.query("ALTER TABLE `upcomingContract` ADD `publicSalePrice` int NULL");
-        await queryRunner.query("ALTER TABLE `upcomingContract` DROP COLUMN `preSalePrice`");
-        await queryRunner.query("ALTER TABLE `upcomingContract` ADD `preSalePrice` int NULL");
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX IDX_DECODEDLOG_TIMESTAMP_DESC`);
+  }
 }
