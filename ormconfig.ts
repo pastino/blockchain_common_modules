95f6c092cdd56ade5d3ac00c4bd6fcd880dfd46e
taejun <joon5006@naver.com>
2023-07-15 12:23:51 +0900

    fix: ormconfing cli 속성 수정

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,9 +1,12 @@
-import { ConnectionOptions } from "typeorm";
+import { ConnectionOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../../../.env.dev' });
+const isNestJs = process.env.APP_TYPE === 'nestjs';
 
-const IS_PRODUCTION = process.env.NODE_ENV === "production";
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 export const ormconfig: ConnectionOptions = {
-  type: "mysql",
+  type: 'mysql',
   host: process.env.HOST,
   port: Number(process.env.MYSQL_PORT),
   username: process.env.USERNAME,
@@ -11,16 +14,16 @@ export const ormconfig: ConnectionOptions = {
   database: process.env.DATABASE,
   // database: IS_PRODUCTION ? process.env.DATABASE : process.env.TEST_DATABASE,
   synchronize: false,
-  logging: ["warn", "error"],
-  charset: "utf8mb4_unicode_ci",
-  entities: [__dirname + "/entities/*.*"],
-  migrations: [__dirname + "/migrations/*.*"],
-  subscribers: [__dirname + "/subscribers/*.*"],
-  cli: {
-    migrationsDir: "src/shared/migrations",
-  },
+  logging: ['warn', 'error'],
+  charset: 'utf8mb4_unicode_ci',
+  entities: [__dirname + '/entities/*.*'],
+  migrations: [__dirname + '/migrations/*.*'],
+  subscribers: [__dirname + '/subscribers/*.*'],
   extra: {},
-  timezone: "Z",
+  timezone: 'Z',
+  ...(!isNestJs && {
+    cli: {
+      migrationsDir: 'src/shared/migrations',
+    },
+  }),
 };
-
-export default ormconfig;
