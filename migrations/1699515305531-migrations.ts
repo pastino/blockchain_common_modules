992d4849f73f005d56d708f497224d59af34e4ed
taejun <joon5006@naver.com>
2023-11-09 17:14:24 +0900

    fix: 트랜잭션 테이블에 hash 인덱스 추가

--- migrations/1699515305531-migrations.ts
+++ migrations/1699515305531-migrations.ts
@@ -1,15 +1,17 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class migrations1699506958093 implements MigrationInterface {
-    name = 'migrations1699506958093'
+export class migrations1699515305531 implements MigrationInterface {
+    name = 'migrations1699515305531'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP INDEX "idx_decodedlog_contract"`);
         await queryRunner.query(`DROP INDEX "idx_trend_time_contract"`);
         await queryRunner.query(`DROP INDEX "idx_opensea_collection_contract"`);
+        await queryRunner.query(`CREATE INDEX "idx_transaction_hash" ON "transaction" ("hash") `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "idx_transaction_hash"`);
         await queryRunner.query(`CREATE INDEX "idx_opensea_collection_contract" ON "openseaCollection" ("contractId") `);
         await queryRunner.query(`CREATE INDEX "idx_trend_time_contract" ON "trendCollection" ("contractId") `);
         await queryRunner.query(`CREATE INDEX "idx_decodedlog_contract" ON "decodedLog" ("contractId") `);
