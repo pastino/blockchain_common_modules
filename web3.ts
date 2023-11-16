2bd8687b0a4e37af4143211e58cfd25906bb6360
taejun <joon5006@naver.com>
2023-11-16 16:32:13 +0900

    fix: ethereum node url 변경

--- web3.ts
+++ web3.ts
@@ -1,5 +1,5 @@
 const Web3 = require("web3");
-const ETH_NODE_HTTP = `http://121.168.75.64`;
+const ETH_NODE_HTTP = `http://121.168.75.64/block/`;
 const ETH_NODE_WS = `ws://121.168.75.64/eth-ws/`;
 
 const web3 = new Web3(new Web3.providers.HttpProvider(ETH_NODE_HTTP));
