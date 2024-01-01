f4c64d3451213d39ee5adab1994aa0c55af1dfe0
김태준 <gimtaejun@gimtaejun-ui-MacBookPro.local>
2024-01-01 13:53:50 +0900

    fix: 시간 테스트 확인을 위한 콘솔

--- modules/transaction.ts
+++ modules/transaction.ts
@@ -151,6 +151,7 @@ export class Transaction {
     decodedLog?: any;
   }) {
     try {
+      console.log(1);
       const { topics, ...logWithoutTopics } = log;
       delete logWithoutTopics?.id;
       const logInputData: any = {};
@@ -163,14 +164,17 @@ export class Transaction {
         logInputData.nft = nftData;
       }
 
+      console.log(2);
       const logData = await getRepository(LogEntity).save({
         transaction,
         ...logWithoutTopics,
         ...logInputData,
       });
+      console.log(3);
 
       if (decodedLog) {
         try {
+          console.log(4);
           const createdDecodeLog = await getRepository(DecodedLog).save({
             ...decodedLog,
             contractAddress: decodedLog.contract,
@@ -185,15 +189,17 @@ export class Transaction {
             gasPrice: transaction.gasPrice,
             gasLimit: transaction.gasLimit,
           });
+          console.log(5);
           await getRepository(LogEntity).update(
             { id: logData.id },
             { decodedLog: createdDecodeLog }
           );
+          console.log(6);
         } catch (e) {
           throw e;
         }
       }
-
+      console.log(7);
       for (let i = 0; i < topics.length; i++) {
         const value = topics[i];
         await getRepository(TopicEntity).save({
@@ -202,6 +208,7 @@ export class Transaction {
           log: logData,
         });
       }
+      console.log(8);
     } catch (e: any) {
       throw e;
     }
@@ -221,13 +228,10 @@ export class Transaction {
       for (let index = 0; index < transactions.length; index++) {
         const transactionHash = transactions[index];
 
-        console.log(1);
         const transactionData = await this.getTransaction(transactionHash);
-        console.log(2);
         const transactionReceipt = await this.getTransactionReceipt(
           transactionHash
         );
-        console.log(3);
 
         const timestamp = this.blockData.timestamp;
         const eventTime = new Date(timestamp * 1000);
@@ -239,7 +243,6 @@ export class Transaction {
           timestamp,
           eventTime,
         };
-        console.log(4);
         const transaction = await getRepository(TransactionEntity).save({
           ...transactionData,
           data: transactionData.input,
@@ -253,7 +256,6 @@ export class Transaction {
           chainId: parseInt(transactionData.chainId, 16) || null,
           ...timeOption,
         });
-        console.log(5);
         const logs = transactionReceipt?.logs;
 
         if (!logs || logs.length === 0) continue;
@@ -263,7 +265,6 @@ export class Transaction {
         );
 
         for (const log of logs) {
-          console.log(1, 6);
           const data = await getIsERC721Event(
             log,
             logs,
@@ -271,9 +272,7 @@ export class Transaction {
             transaction.hash
           );
           const decodedData = data.decodedData;
-          console.log(1, 7);
           if (data.isERC721Event) {
-            console.log(2, 8);
             const contractAddress = decodedData?.contract;
             const result = await this.createContractAndNFT({
               transaction,
@@ -291,7 +290,6 @@ export class Transaction {
               decodedLog: decodedData || null,
             });
           } else {
-            console.log(2, 9);
             await this.createLog({
               log,
               transaction,
@@ -300,13 +298,10 @@ export class Transaction {
         }
       }
 
-      console.log(10);
-
       await getRepository(BlockNumberEntity).update(
         { id: this.blockNumber.id },
         { isNFTCompletedUpdate: true }
       );
-      console.log(11);
       return { isSuccess: true };
     } catch (e) {
       throw e;
