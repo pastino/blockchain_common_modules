4b9576377525e5c380fded013be6910648dd3f83
김태준 <gimtaejun@gimtaejun-ui-MacBookPro.local>
2024-01-12 19:59:58 +0900

    fix: 이미지 생성 콘솔

--- downloadNFTImage.ts
+++ downloadNFTImage.ts
@@ -275,6 +275,7 @@ export const downloadImage = async ({
     } else {
       fs.writeFileSync(path.join(thumbnailPath, hashedFileName), imageData);
     }
+    console.log(contractAddress, tokenId, "이미지 생성 완료");
     return { isSuccess: true, message: "", hashedFileName };
   } catch (error: any) {
     return { isSuccess: false, message: error.message, hashedFileName: "" };
