a9650096b1af88a815d22ac6d5520b1745aca810
김태준 <gimtaejun@gimtaejun-ui-MacBookPro.local>
2024-03-08 15:20:42 +0900

    fix: 이더리움 노드 url 변경

--- web3.ts
+++ web3.ts
@@ -1,7 +1,7 @@
 const Web3 = require("web3");
 
-const ETH_NODE_HTTP = process.env.ETH_NODE_HTTP;
-const ETH_NODE_WS = process.env.ETH_NODE_WS;
+const ETH_NODE_HTTP = `${process.env.AWS_ETH_NODE_HTTP}/?billingtoken=${process.env.AWS_ETH_NODE_TOKEN}`;
+const ETH_NODE_WS = `${process.env.ETH_NODE_WS}/?billingtoken=${process.env.AWS_ETH_NODE_TOKEN}`;
 
 const web3 = new Web3(new Web3.providers.HttpProvider(ETH_NODE_HTTP));
 const web_socket3 = new Web3(new Web3.providers.WebsocketProvider(ETH_NODE_WS));
